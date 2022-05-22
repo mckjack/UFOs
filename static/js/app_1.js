94a2f6a2352259e1738698106020a9bf8b05d0b1
Jack McKinnon <jackmckinnon13@hotmail.com>
2022-05-22 16:02:53 -0400

    Adding Module 11 Challenge

---
+++ static/js/app_1.js
@@ -0,0 +1,53 @@
+// import the data from data.js
+const tableData= data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Building function to add data to tables
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
+
+
+// Adding a function to filter the data by clicking a button 
+function handleClick(){
+    let date = d3.select('#datetime').property("value");
+    let filteredData = tableData;
+    
+    // pseudocode practice
+    // if (a date is entered) {
+        // Filter the default data to show only the date entered
+    // };
+
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+    // Rebuild table using filtered data
+    buildTable(filteredData);
+}
+
+// Listen to the click button 
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
+
+
